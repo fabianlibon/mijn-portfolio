48411cc79775713c428d8e2086722f74c12eb676
fabianlibon <libonfabian@gmail.com>
2025-12-10 15:46:50 +0100

    mail

--- js/map.js
+++ js/map.js
@@ -1,11 +1,11 @@
-let map = L.map('apMap').setView([51.2963579, 4.5749117], 16);
+let map = L.map('map').setView([51.2963579, 4.5749117], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-let bounds = [[51.3030985, 4.5518612], [51.2902816, 4.5925677]];
+let bounds = [[51.312487, 4.5420696], [51.2877603, 4.5955331]];
 
 L.rectangle(bounds, { color: "#ff6600", weight: 1 }).addTo(map);
 
